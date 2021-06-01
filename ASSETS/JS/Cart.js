ba45a67a28eeafede8eefea5f80de6f0a4d4e815
quantudev211154 <quantu211154@gmail.com>
2021-06-01 15:01:03 +0700

    Hoan thanh phan Cart

--- ASSETS/JS/Cart.js
+++ ASSETS/JS/Cart.js
@@ -1,40 +1,111 @@
-// const activeHideCart = document.getElementById('expand')
-// const listItem = document.querySelector('.demoCart-listItem')
+const activeHideCart = document.getElementById('expand')
+const listItem = document.querySelector('.demoCart-listItem')
+const listItemChild = document.querySelectorAll('.demoCart-listItem-item')
+const deleteItem = document.querySelectorAll('.deletePrd')
+const listSubtractIcon = document.querySelectorAll('.subtractIcon')
+const listAddIcon = document.querySelectorAll('.addIcon')
+const listQuantityInp = document.querySelectorAll('.quantity')
+const listPrice = document.querySelectorAll('.price')
+const listTotalPrice = document.querySelectorAll('.totalPrice')
 
-// ;(function expandCart(){
-//   activeHideCart.onclick = () => {
-//     if (listItem.classList.contains('active')){
-//       listItem.classList.remove('active')
-//       activeHideCart.classList.remove('active')
-//     }
-//     else{
-//       listItem.classList.add('active')
-//       activeHideCart.classList.add('active')
-//     }
-//   }
-// })()
+;(function expandCart(){
+  activeHideCart.onclick = () => {
+    if (listItem.classList.contains('active')){
+      listItem.classList.remove('active')
+      activeHideCart.classList.remove('active')
+      listItemChild.forEach(item => {
+        item.classList.remove('active')
+      });
+    }
+    else{
+      listItem.classList.add('active')
+      activeHideCart.classList.add('active')
+      listItemChild.forEach(item => {
+        item.classList.add('active')
+      });
+    }
+  }
+})()
 
-var x = "27499000"
+;(function disableInpQuantity(){
+  listQuantityInp.forEach(inp => {
+    inp.disabled = true
+    inp.value = '1'
+  });
+})()
 
-function convert(x){
-  var y = ''
+ function convertFromIntToString(source){
+  var tmp = ''
   var count = 0
-  var re = ''
-  for (let i = x.length - 1; i >= 0; i--){
+  var result = ''
+  var count2 = 0
+  for (let i = source.length - 1; i >= 0; i--){
     if (count != 3){
-      y += x.charAt(i)
-      count++
+      tmp += source.charAt(i)
+      count++;
+      count2++
     }
-    if (count == 3){
-      count = 0
-      y += '.'
+    if (count == 3 && count2 < source.length){
+        count = 0
+        tmp += '.'
     }
   }
-  for (let i = y.length - 1; i > 0; i--){
-    re += y.charAt(i)
+  for (let i = tmp.length - 1; i >= 0; i--){
+    result += tmp.charAt(i)
   }
-  return re
+  return result
 }
 
-var pr = convert(x)
-console.log(pr)
+function convertFromStringToInt(source){
+  var result = ''
+  for (let i = 0; i < source.length; ++i){
+    if (source.charAt(i) != '.'){
+      result += source.charAt(i)
+    }
+  }
+  return parseInt(result)
+}
+
+;(function editQuantityValue(){
+  for (let i = 0; i < listQuantityInp.length; ++i){
+    listAddIcon[i].onclick = () => {
+      if (listQuantityInp[i].value == '5'){
+        listAddIcon[i].classList.add('active')
+      }
+      else{
+        var currenQuan = parseInt(listQuantityInp[i].value) + 1
+        listQuantityInp[i].value = currenQuan
+        listSubtractIcon[i].classList.remove('active')
+        var prdPrice = convertFromStringToInt(listPrice[i].innerHTML)
+        var currentTotalPrice = prdPrice * currenQuan
+        var convertTotalPrice = convertFromIntToString(currentTotalPrice + '')
+        listTotalPrice[i].innerHTML = convertTotalPrice
+      }
+    }
+    listSubtractIcon[i].onclick = () => {
+      if (listQuantityInp[i].value == '1'){
+        listSubtractIcon[i].classList.add('active')
+        listAddIcon[i].classList.remove('active')
+      }
+      else{
+        var currenQuan = parseInt(listQuantityInp[i].value) - 1
+        listQuantityInp[i].value = currenQuan
+        listAddIcon[i].classList.remove('active')
+        var prdPrice = convertFromStringToInt(listPrice[i].innerHTML)
+        var currentTotalPrice = prdPrice * currenQuan
+        var convertTotalPrice = convertFromIntToString(currentTotalPrice + '')
+        listTotalPrice[i].innerHTML = convertTotalPrice
+      }
+    }
+  }
+})()
+
+;(function deletePrd(){
+  for (let i = 0; i < listItemChild.length; i++){
+    deleteItem[i].onclick = () => {
+      listItem.removeChild(listItem.childNodes[i + 1])
+    }
+  }
+  if (listItemChild.length == 0)
+    alert('Gio hang rong')
+})()
